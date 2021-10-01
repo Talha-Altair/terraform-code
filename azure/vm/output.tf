6f585760a0dc92e301fd483cf29b8d79639f01bd
Talha-Altair <cooltalha27@gmail.com>
2021-10-01 19:29:09 +0530

    Updates for 2021-10-01 19:29:09

--- azure/vm/output.tf
+++ azure/vm/output.tf
@@ -2,3 +2,7 @@ output "tls_private_key" {
     value = tls_private_key.example_ssh.private_key_pem 
     sensitive = true
 }
+
+output "instance ip" {
+    value = azurerm_linux_virtual_machine.public_ip_address_id
+}
