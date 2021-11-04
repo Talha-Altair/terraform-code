ee097e639c5ad70e19bce3b0ef2106c891a78dbb
Talha-Altair <cooltalha27@gmail.com>
2021-11-04 16:17:41 +0530

    Updates for 2021-11-04 16:17:41

---
+++ azure/k8s/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "altair"
+  location            = "East US"
+  resource_group_name = "talha-rg"
+  dns_prefix          = "altair-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    talha = "altair"
+  }
+}
---
+++ azure/k8s/variables.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  description = "subscription_id"
+}
+
+variable "client_id" {
+  description = "client_id"
+}
+
+variable "client_secret" {
+  description = "client_secret"
+}
+
+variable "tenant_id" {
+  description = "tenant_id"
+}
